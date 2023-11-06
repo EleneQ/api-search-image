01c4c5ea481ea190220342ea43551e203c3ecd73
Elene Qaldani <eleneqaldani1@gmail.com>
2023-11-06 16:28:54 +0400

    added base utl to vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //all static assets are routed through this base url
+  base: "/api-search-image/",
   plugins: [react()],
-})
+});
